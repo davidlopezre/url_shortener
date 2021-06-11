72a00ffe08fe7041b970096b0f76e1dc73e286ae
David Lopez <davidlopezre@gmail.com>
2021-06-12 00:29:42 +1000

    move db initialisation to a separate function

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,32 @@ use rouille::{Response, router};
 
 
 fn main() {
+    init();
     println!("Now listening on localhost:8000");
-   rouille::start_server("localhost:8000", move |request| {
+    rouille::start_server("localhost:8000", move |request| {
+    
+    router!(request,
+        (GET) (/api/url/{location: String}) => {
+            let conn = Connection::open("url_shortener.db").unwrap();
+
+            match Url::fetch_from_db(&conn, location) {
+                Ok(Some(u)) => rouille::Response::json(&u),
+                Ok(None) => Response::empty_404(),
+                Err(e) => {
+                    println!("error: {}", e.to_string());
+                    let mut response = Response::text("server error");
+                    response.status_code = 500;
+                    return response;
+                }
+            }     
+        },
+
+        _ => rouille::Response::empty_404()
+    )
+});
+}
+
+fn init() {
     let conn = Connection::open("url_shortener.db");
     if let Err(e) = conn {
         panic!("{}", e.to_string());
@@ -24,24 +48,4 @@ fn main() {
     ) {
         panic!("{}", e.to_string());
     }
-    router!(request,
-        (GET) (/api/url/{location: String}) => {
-            println!("u32 {:?}", location);
-
-            match Url::fetch_from_db(&conn, location) {
-                Ok(Some(u)) => rouille::Response::json(&u),
-                Ok(None) => Response::empty_404(),
-                Err(e) => {
-                    let mut response = Response::text(e.to_string());
-                    response.status_code = 400;
-                    return response;
-                }
-            }     
-        },
-
-        // The code block is called if none of the other blocks matches the request.
-        // We return an empty response with a 404 status code.
-        _ => rouille::Response::empty_404()
-    )
-});
 }
