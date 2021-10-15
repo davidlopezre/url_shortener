f08decc33debb1ed6eddb8c77906625877f65c18
David Lopez <dreyes@zendesk.com>
2021-10-15 15:28:34 +1100

    move app initialisation to main file

--- src/config.rs
+++ src/config.rs
@@ -1,5 +1,5 @@
 pub struct Config {
-    db_path: String,
+    pub db_path: String,
 }
 
 impl Config {
@@ -10,24 +10,4 @@ impl Config {
             db_path: db_path.to_string(),
         };
     }
-}
-
-pub fn initialise_app(cfg: &Config) -> Result<(), String> {
-    let conn = rusqlite::Connection::open(&cfg.db_path);
-    if let Err(e) = conn {
-        return Err(e.to_string());
-    }
-    let conn = conn.unwrap();
-
-    if let Err(e) = conn.execute(
-        "CREATE TABLE IF NOT EXISTS url (
-                  location        TEXT PRIMARY KEY,
-                  target          TEXT NOT NULL,
-                  created_at      TEXT
-                  )",
-        [],
-    ) {
-        return Err(e.to_string());
-    }
-    Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,53 @@
 use core::panic;
 
-use rusqlite::{Connection};
-use url_shortener::Url;
-use rouille::{Response, router};
+use rouille::{router, Response};
+use rusqlite::Connection;
 use url_shortener::config;
+use url_shortener::Url;
 
 fn main() {
     let cfg = config::Config::new();
-    config::initialise_app(&cfg).unwrap_or_else(|e|panic!("failed to initialise_app: {}", e));
+    initialise_app(&cfg).unwrap_or_else(|e| panic!("failed to initialise_app: {}", e));
 
     println!("Now listening on localhost:8000");
     rouille::start_server("localhost:8000", move |request| {
-    
-    router!(request,
-        (GET) (/api/url/{location: String}) => {
-            let conn = Connection::open("url_shortener.db").unwrap();
+        router!(request,
+            (GET) (/api/url/{location: String}) => {
+                let conn = Connection::open("url_shortener.db").unwrap();
 
-            match Url::fetch_from_db(&conn, location) {
-                Ok(Some(u)) => rouille::Response::json(&u),
-                Ok(None) => Response::empty_404(),
-                Err(e) => {
-                    println!("error: {}", e.to_string());
-                    let mut response = Response::text("server error");
-                    response.status_code = 500;
-                    return response;
+                match Url::fetch_from_db(&conn, location) {
+                    Ok(Some(u)) => rouille::Response::json(&u),
+                    Ok(None) => Response::empty_404(),
+                    Err(e) => {
+                        println!("error: {}", e.to_string());
+                        let mut response = Response::text("server error");
+                        response.status_code = 500;
+                        return response;
+                    }
                 }
-            }     
-        },
+            },
 
-        _ => rouille::Response::empty_404()
-    )
-});
+            _ => rouille::Response::empty_404()
+        )
+    });
+}
+
+pub fn initialise_app(cfg: &config::Config) -> Result<(), String> {
+    let conn = rusqlite::Connection::open(&cfg.db_path);
+    if let Err(e) = conn {
+        return Err(e.to_string());
+    }
+    let conn = conn.unwrap();
+
+    if let Err(e) = conn.execute(
+        "CREATE TABLE IF NOT EXISTS url (
+                  location        TEXT PRIMARY KEY,
+                  target          TEXT NOT NULL,
+                  created_at      TEXT
+                  )",
+        [],
+    ) {
+        return Err(e.to_string());
+    }
+    Ok(())
 }
