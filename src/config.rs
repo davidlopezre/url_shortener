a78eb09c753cd46aba1f525abfc4722c29d56986
David Lopez <dreyes@zendesk.com>
2021-10-15 16:23:32 +1100

    add tests for config mod

--- src/config.rs
+++ src/config.rs
@@ -11,3 +11,24 @@ impl Config {
         };
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::Config;
+    use std::env;
+
+    #[test]
+    fn test_new_config_db_path_env_var() -> Result<(), String> {
+        env::set_var("URL_SHORTENER_DB_PATH", "test_path.db");
+        let cfg = Config::new();
+        assert_eq!("test_path.db", cfg.db_path);
+        Ok(())
+    }
+
+    #[test]
+    fn test_new_config_default_db_path() -> Result<(), String> {
+        let cfg = Config::new();
+        assert_eq!("url_shortener.db", cfg.db_path);
+        Ok(())
+    }
+}
